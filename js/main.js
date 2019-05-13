15f1ebb58268822e2246cf74ea4c4bf94702127b
stepanabgaryan <stepanabgaryan@gmail.com>
2019-05-13 15:50:48 +0400

    Changed style and information. Added Portfolio part.

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Stepan Abgaryan.", " An Engineer.", "Web Developer."],
       typeSpeed: 100,
       loop: true,
     });
